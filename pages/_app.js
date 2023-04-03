f03fda30f7ec0e1bcbe2368da65d4dd2eb900987
Anja Siewert <anja.siewert@live.de>
2023-04-03 11:19:57 +0200

    fetch data

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  const { data, error, isLoading } = useSWR(
+    "https://restcountries.com/v3.1/all",
+    fetcher
+  );
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "is loading...";
+
+  console.log(data);
   return (
     <>
       <GlobalStyle />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
+  return <></>;
 }
