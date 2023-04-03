a0a69fd3426b7e64122fde1f076cc0b5007d6f56
Anja Siewert <anja.siewert@live.de>
2023-04-03 12:05:25 +0200

    sort country list by alphabet

--- components/CountriesList/CountriesList.js
+++ components/CountriesList/CountriesList.js
@@ -2,9 +2,11 @@ export default function CountriesList({ countries }) {
   return (
     <>
       <ul>
-        {countries.map((country) => (
-          <li key={country.name.common}>{country.name.common}</li>
-        ))}
+        {countries
+          .sort((a, b) => a.name.common.localeCompare(b.name.common))
+          .map((country) => (
+            <li key={country.name.common}>{country.name.common}</li>
+          ))}
       </ul>
     </>
   );
