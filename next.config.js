26328bcd40822808101303aa820712583c2d1491
Anja Siewert <anja.siewert@live.de>
2023-04-05 15:30:56 +0200

    add image to homepage

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     styledComponents: true,
   },
   reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+import Header from "../components/Header/Header";
+import Image from "next/image";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+`;
+
 export default function HomePage() {
   return (
     <>
-      <p>Homepage will be added in next US</p>
+      <Header headline="globehopper" />
+      <StyledDiv>
+        <Image
+          src="/../public/assets/worldmap.jpg"
+          height={250}
+          width={250}
+          alt="worldmap"
+        />
+      </StyledDiv>
     </>
   );
 }
