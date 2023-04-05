bad1e77150be3d0e144219e4a904ae770ba26d97
Anja Siewert <anja.siewert@live.de>
2023-04-05 11:26:50 +0200

    delete dummy links

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Link from "next/link";
+import Header from "../components/Header/Header";
 
 export default function HomePage() {
   return (
     <>
-      <Link href="/list">Go to List of countries</Link>
-      <br />
-      <Link href="/visited">Go to List of visited countries</Link>
+      <Header headline="globehopper" />
     </>
   );
 }
