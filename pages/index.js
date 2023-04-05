fd1b740969034d23cdb6ad3eddc06c3167c15d1c
Anja Siewert <anja.siewert@live.de>
2023-04-05 11:42:32 +0200

    add dummy sentence to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import Header from "../components/Header/Header";
-
 export default function HomePage() {
   return (
     <>
-      <Header headline="globehopper" />
+      <p>Homepage will be added in next US</p>
     </>
   );
 }
