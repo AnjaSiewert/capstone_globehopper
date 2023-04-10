a5fec2d13e20b9654830eccbe613552f691b72f8
Anja Siewert <anja.siewert@live.de>
2023-04-10 21:55:26 +0200

    add counter all continents

--- components/CounterContinent/CounterContinent.js
+++ components/CounterContinent/CounterContinent.js
@@ -1,25 +1,69 @@
 import StyledDiv from "../StyledDiv";
 
 export default function CounterContinent({ countries, countriesInfo }) {
-  const countriesInEurope = countries.filter(
-    (country) => country.region === "Europe"
-  );
+  console.log(countries);
+  const allContinents = [
+    {
+      name: "Europe",
+      countries: countries.filter((country) => country.region === "Europe"),
+    },
+    {
+      name: "Asia",
+      countries: countries.filter((country) => country.region === "Asia"),
+    },
+    {
+      name: "Africa",
+      countries: countries.filter((country) => country.region === "Africa"),
+    },
+    {
+      name: "North America",
+      countries: countries.filter(
+        (country) =>
+          country.region === "Americas" && country.subregion === "North America"
+      ),
+    },
+    {
+      name: "South America",
+      countries: countries.filter(
+        (country) =>
+          country.region === "Americas" && country.subregion == "South America"
+      ),
+    },
+    {
+      name: "Oceania",
+      countries: countries.filter((country) => country.region === "Oceania"),
+    },
+    {
+      name: "Antarctica",
+      countries: countries.filter((country) => country.region === "Antarctic"),
+    },
+  ];
 
-  const countVisitedCountriesInEurope = countriesInEurope.filter((country) =>
-    countriesInfo
-      .filter((info) => info.isVisited)
-      .find((info) => info.name === country.name.common)
-  );
+  const continentCounts = allContinents.map((continent) => {
+    const visitedCountries = continent.countries.filter((country) =>
+      countriesInfo
+        .filter((info) => info.isVisited)
+        .find((info) => info.name === country.name.common)
+    );
+
+    return {
+      name: continent.name,
+      visitedCount: visitedCountries.length,
+      totalCount: continent.countries.length,
+    };
+  });
 
   return (
     <>
-      <StyledDiv>
-        <h2>Europe</h2>
-        <p>
-          I have been in <strong>{countVisitedCountriesInEurope.length}</strong>{" "}
-          countries of {countriesInEurope.length} countries in Europe
-        </p>
-      </StyledDiv>
+      {continentCounts.map((continent) => (
+        <StyledDiv key={continent.name}>
+          <h2>{continent.name}</h2>
+          <p>
+            I have been in <strong>{continent.visitedCount}</strong> countries
+            of {continent.totalCount} countries in {continent.name}
+          </p>
+        </StyledDiv>
+      ))}
     </>
   );
 }
