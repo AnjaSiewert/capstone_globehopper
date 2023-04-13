625e94784e606f165aae225fbd95c31f90d89b9c
Anja Siewert <anja.siewert@live.de>
2023-04-13 11:35:02 +0200

    fix Antarctica bug

--- components/CountryDetails/index.js
+++ components/CountryDetails/index.js
@@ -1,57 +1,65 @@
 import StyledFlag from "../StyledFlag";
 import StyledDiv from "../StyledDiv";
+import Link from "next/link";
 
 export default function CountryDetails({ selectedCountry }) {
-  //console.log(countries);
-  console.log("selectedCountyInCountryDetails", selectedCountry);
   function addSeparators(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   }
   if (!selectedCountry) return <h2>is Loading</h2>;
-  const languagesWithSeparators = Object.values(selectedCountry.languages).join(
-    ", "
-  );
+  const languagesWithSeparators =
+    selectedCountry.languages?.length > 0 &&
+    Object.values(selectedCountry.languages).join(", ");
+
+  const timezoneWithSeparators = selectedCountry.timezones?.join(", ");
 
   return (
-    <StyledDiv isOnDetailsPage>
-      <h1>{selectedCountry.name.common}</h1>
-      <p>
-        <strong>Continent:</strong> {selectedCountry.continents}
-      </p>
-      <p>
-        <strong>Subregion:</strong> {selectedCountry.subregion}
-      </p>
-      <p>
-        <strong>Capital:</strong> {selectedCountry.capital}
-      </p>
-      <p>
-        <strong>Population:</strong> {addSeparators(selectedCountry.population)}
-      </p>
-      <p>
-        <strong>Language: </strong>
-        {languagesWithSeparators}
-      </p>
-
-      {
+    <>
+      <Link href="/list">🔙</Link>
+      <StyledDiv isOnDetailsPage>
+        <h1>{selectedCountry.name.common}</h1>
         <p>
-          <strong>Currency: </strong>
-          {Object.values(selectedCountry.currencies).map((currency, index) => (
-            <span key={index}>
-              {currency.name} <br />
-              <strong>Symbol: </strong>
-              {currency.symbol}
-            </span>
-          ))}
+          <strong>Continent:</strong> {selectedCountry.continents}
         </p>
-      }
-      <p>
-        <strong>Timezone:</strong> {selectedCountry.timezones}
-      </p>
-      <p>
-        <strong>UN Member:</strong> {selectedCountry.unMember ? "Yes" : "No"}
-      </p>
+        <p>
+          <strong>Subregion:</strong> {selectedCountry.subregion}
+        </p>
+        <p>
+          <strong>Capital:</strong> {selectedCountry.capital}
+        </p>
+        <p>
+          <strong>Population:</strong>{" "}
+          {addSeparators(selectedCountry.population)}
+        </p>
+        <p>
+          <strong>Language: </strong>
+          {languagesWithSeparators}
+        </p>
 
-      <StyledFlag>{selectedCountry.flag}</StyledFlag>
-    </StyledDiv>
+        {
+          <p>
+            <strong>Currency: </strong>
+            {selectedCountry.currencies?.length > 0 &&
+              Object.values(selectedCountry.currencies).map(
+                (currency, index) => (
+                  <span key={index}>
+                    {currency.name} <br />
+                    <strong>Symbol: </strong>
+                    {currency.symbol}
+                  </span>
+                )
+              )}
+          </p>
+        }
+        <p>
+          <strong>Timezone:</strong> {timezoneWithSeparators}
+        </p>
+        <p>
+          <strong>UN Member:</strong> {selectedCountry.unMember ? "Yes" : "No"}
+        </p>
+
+        <StyledFlag>{selectedCountry.flag}</StyledFlag>
+      </StyledDiv>
+    </>
   );
 }
