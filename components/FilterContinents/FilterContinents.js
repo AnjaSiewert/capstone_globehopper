2ba8b6babb415cfd2577f5cd1340c8e2a94e22db
Anja Siewert <anja.siewert@live.de>
2023-04-21 22:39:03 +0200

    add central america to filter

--- components/FilterContinents/FilterContinents.js
+++ components/FilterContinents/FilterContinents.js
@@ -1,54 +1,60 @@
 export default function FilterContinents({ countries, setFilteredCountries }) {
-  console.log(countries);
-
   const handleSelect = (event) => {
-    const selectedContinent = event.target.value;
-    if (selectedContinent === "all") {
-      setFilteredCountries(countries);
-    } else if (selectedContinent === "africa") {
+    if (event.target.value === "africa") {
       setFilteredCountries(
         countries.filter((country) => {
           return country.region === "Africa";
         })
       );
-    } else if (selectedContinent === "asia") {
+    } else if (event.target.value === "asia") {
       setFilteredCountries(
         countries.filter((country) => {
           return country.region === "Asia";
         })
       );
-    } else if (selectedContinent === "europe") {
+    } else if (event.target.value === "europe") {
       setFilteredCountries(
         countries.filter((country) => {
           return country.region === "Europe";
         })
       );
-    } else if (selectedContinent === "north america") {
+    } else if (event.target.value === "north america") {
       setFilteredCountries(
         countries.filter((country) => {
-          return country.continents.includes("North America");
+          return (
+            country.continents.includes("North America") &&
+            country.subregion !== "Central America"
+          );
         })
       );
-    } else if (selectedContinent === "oceania") {
+    } else if (event.target.value === "oceania") {
       setFilteredCountries(
         countries.filter((country) => {
           return country.region === "Oceania";
         })
       );
-    } else if (selectedContinent === "south america") {
+    } else if (event.target.value === "south america") {
       setFilteredCountries(
         countries.filter((country) => {
           return country.continents.includes("South America");
         })
       );
+    } else if (event.target.value === "Central America") {
+      setFilteredCountries(
+        countries.filter((country) => {
+          return country.subregion === "Central America";
+        })
+      );
     }
   };
+
   return (
     <>
       <select onChange={handleSelect}>
-        <option value="all">All</option>
+        <option value="">Filter by Continent</option>
         <option value="africa">Africa</option>
         <option value="asia">Asia</option>
+        <option value="Central America">Central America</option>
         <option value="europe">Europe</option>
         <option value="north america">North America</option>
         <option value="oceania">Oceania</option>
