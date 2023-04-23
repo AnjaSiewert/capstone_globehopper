3d78d0c45e0cefb38add95426794da8332fe4f14
Anja Siewert <anja.siewert@live.de>
2023-04-23 12:56:26 +0200

    add missing timezones

--- components/CountryDetails/index.js
+++ components/CountryDetails/index.js
@@ -25,47 +25,43 @@ export default function CountryDetails({ selectedCountry }) {
   const timezoneWithSeparators = selectedCountry.timezones?.join(", ");
 
   // find a solution for countries with multiple timezones
-  // UTC :30 are missing
-
-  /* "UTCs missing:"
-
-"UTC+03:30"
-"UTC+04:30"
-"UTC+05:30"
-"UTC+05:45"
-"UTC+06:30"
-"UTC+09:30"
-"UTC+10:30"
-"UTC+11:30"
-"UTC+12:45"
-"UTC+13:00"
-"UTC+14:00"
-"UTC-03:30"
-"UTC-09:30" */
 
   function getLocalTime(selectedCountry) {
     const timeZoneMap = {
       "UTC+01:00": "Europe/Paris",
       "UTC+02:00": "Europe/Helsinki",
       "UTC+03:00": "Europe/Moscow",
+      "UTC+03:30": "Asia/Tehran",
       "UTC+04:00": "Asia/Dubai",
+      "UTC+04:30": "Asia/Kabul",
       "UTC+05:00": "Asia/Karachi",
+      "UTC+05:30": "Asia/Calcutta",
+      "UTC+05:45": "Asia/Katmandu",
       "UTC+06:00": "Asia/Dhaka",
+      "UTC+06:30": "Asia/Rangoon",
       "UTC+07:00": "Asia/Bangkok",
       "UTC+08:00": "Asia/Shanghai",
       "UTC+09:00": "Asia/Tokyo",
+      "UTC+09:30": "Australia/Darwin",
       "UTC+10:00": "Australia/Sydney",
+      "UTC+10:30": "Australia/Lord_Howe",
       "UTC+11:00": "Pacific/Guadalcanal",
+      "UTC+11:30": "Pacific/Norfolk",
       "UTC+12:00": "Pacific/Auckland",
+      "UTC+12:45": "Pacific/Chatham",
+      "UTC+13:00": "Pacific/Tongatapu",
+      "UTC+14:00": "Pacific/Kiritimati",
       "UTC-01:00": "Atlantic/Azores",
       "UTC-02:00": "America/Noronha",
       "UTC-03:00": "America/Sao_Paulo",
+      "UTC-03:30": "America/St_Johns",
       "UTC-04:00": "America/Caracas",
       "UTC-05:00": "America/Bogota",
       "UTC-06:00": "America/Mexico_City",
       "UTC-07:00": "America/Phoenix",
       "UTC-08:00": "America/Los_Angeles",
       "UTC-09:00": "America/Anchorage",
+      "UTC-09:30": "Pacific/Marquesas",
       "UTC-10:00": "Pacific/Honolulu",
       "UTC-11:00": "Pacific/Midway",
       "UTC-12:00": "Pacific/Kwajalein",
