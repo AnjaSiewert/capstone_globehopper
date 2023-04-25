fab494262a3469e4c0e11097a83e4090566489a0
Anja Siewert <anja.siewert@live.de>
2023-04-25 15:14:43 +0200

    adjust navbar items

--- components/StyledButton.js
+++ components/StyledButton.js
@@ -5,7 +5,7 @@ const StyledButton = styled.button`
   ${(props) =>
     props.isHidingForm &&
     css`
-      margin: 0.5rem 6rem 0.5rem 6rem;
+      margin: 0.5rem 6rem 0.5rem 5.2rem;
     `}
 `;
 
--- components/StyledFooter.js
+++ components/StyledFooter.js
@@ -3,9 +3,11 @@ import styled from "styled-components";
 const StyledFooter = styled.footer`
   display: flex;
   justify-content: space-around;
+  align-items: center;
   position: fixed;
   bottom: 0;
   width: 100%;
+  height: 4rem;
   background-color: var(--tourquoise);
 `;
 
