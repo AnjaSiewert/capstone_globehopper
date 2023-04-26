d7d6efca4dec0896632bc8adb136ac32ee5ae258
Anja Siewert <anja.siewert@live.de>
2023-04-26 11:46:50 +0200

    add effect to Navbar

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -6,19 +6,40 @@ import StyledFooter from "../StyledFooter";
 import Star from "../../public/assets/star.svg";
 
 export default function Navigation() {
+  const activeStyles = { width: "60px", height: "60px" };
+  const inactiveStyles = { width: "35px", height: "35px" };
+
+  function isActive(href) {
+    if (typeof window !== "undefined") {
+      return window.location.pathname === href;
+    }
+  }
+
   return (
     <StyledFooter>
       <Link aria-label="go to homepage" href="/">
-        <Home aria-label="home" />
+        <Home
+          aria-label="home"
+          style={isActive("/") ? activeStyles : inactiveStyles}
+        />
       </Link>
       <Link aria-label="go to listpage" href="/list">
-        <Earth aria-label="listpage" />
+        <Earth
+          aria-label="listpage"
+          style={isActive("/list") ? activeStyles : inactiveStyles}
+        />
       </Link>
       <Link aria-label="go to visitedpage" href="/visited">
-        <Map aria-label="visitedpage" />
+        <Map
+          aria-label="visitedpage"
+          style={isActive("/visited") ? activeStyles : inactiveStyles}
+        />
       </Link>
       <Link aria-label="go to favoritespage" href="/favorites">
-        <Star aria-label="favoritespage" />
+        <Star
+          aria-label="favoritespage"
+          style={isActive("/favorites") ? activeStyles : inactiveStyles}
+        />
       </Link>
     </StyledFooter>
   );
