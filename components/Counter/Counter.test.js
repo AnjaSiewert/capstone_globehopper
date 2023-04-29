4edd623cfc80dea039d92cb728255b6aaf835cd8
Anja Siewert <anja.siewert@live.de>
2023-04-29 18:37:00 +0200

    add test headline rendering

--- components/Counter/Counter.test.js
+++ components/Counter/Counter.test.js
@@ -1,6 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Counter from ".";
 
+test("renders a headline", () => {
+  const countries = [{ name: "Canada" }];
+  const countriesInfo = [{ name: "Canada", isVisited: true }];
+  render(<Counter countries={countries} countriesInfo={countriesInfo} />);
+  const heading = screen.getByRole("heading", { name: /the world/i });
+  expect(heading).toBeInTheDocument();
+});
+
 test("displays the correct number of visited countries", async () => {
   const countries = [
     { name: "Canada" },
